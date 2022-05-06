d72a78fe0bf20b8d8a6016561070e372c42a3940
GaleArt <GaleguzovAA@gmail.com>
2022-05-06 22:59:45 +0300

    rework app

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://galeart.github.io/GaleArtWeb/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Huec
         </a>
       </header>
     </div>
